b2c8adc920c1302f9cfb10728e3ea1cf81a5492d
Nikita Bykov <v-nibyko@microsoft.com>
2021-05-14 09:35:35 +0300

    test

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dotnet_setup_test
+{
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
